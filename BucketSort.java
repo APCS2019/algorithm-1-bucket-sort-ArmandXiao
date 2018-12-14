8827f873439d7e4202291297a89fa35416f69fd3
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-14 14:56:45 +0800

    Okay

--- BucketSort.java
+++ BucketSort.java
@@ -1,21 +1,21 @@
 public static void inOrder( int[] a){
-        int max = a[0];
-        for(int i = 1; i< a.length; i++){
-          if(max<a[i])
-            max = a[i];
-        }
-        int[] temp = new int[max];
+  int max = a[0];
+  for(int i = 1; i< a.length; i++){
+      if(max<a[i])
+      max = a[i];
+  }
+  int[] temp = new int[max+1];
         
-        for(int i = 0; i< a.length; i++){
-            temp[a[i]] += 1;
-        }
+  for(int i = 0; i< a.length; i++){
+     temp[a[i]] += 1;
+  }
         
-        for(int i = 0; i< temp.length; i++){
-          if(temp[i] != 0){
-            for( int j = 0; j< temp[i]; j++){
-              system.out.println(j +" ");
-            }
-          }
-        }
-      }
+  for(int i = 0; i< temp.length; i++){
+    if(temp[i] != 0){
+      for( int j = 0; j< temp[i]; j++){
+          System.out.println(i +" ");
+       }
+     }
+   }
+}
    
