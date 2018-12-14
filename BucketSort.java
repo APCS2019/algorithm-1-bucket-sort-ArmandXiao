bc343293e5f3f01f349df237a05026c8a5378a7d
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-14 14:51:24 +0800

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -1,1 +1,21 @@
-
+public static void inOrder( int[] a){
+        int max = a[0];
+        for(int i = 1; i< a.length; i++){
+          if(max<a[i])
+            max = a[i];
+        }
+        int[] temp = new int[max];
+        
+        for(int i = 0; i< a.length; i++){
+            temp[a[i]] += 1;
+        }
+        
+        for(int i = 0; i< temp.length; i++){
+          if(temp[i] != 0){
+            for( int j = 0; j< temp[i]; j++){
+              system.out.println(j +" ");
+            }
+          }
+        }
+      }
+   
